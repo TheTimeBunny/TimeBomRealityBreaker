b1c1e1d64335550b711daa9cacef7f5bca3c954a
Stephen Frey <Sfreyjr@gmail.com>
2024-01-28 01:23:36 -0500

    Add project files.

---
+++ TimeBomRealityBreaker.cpp
@@ -0,0 +1,11 @@
+﻿// TimeBomRealityBreaker.cpp : Defines the entry point for the application.
+//
+
+#include "TimeBomRealityBreaker.h"
+
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello World!";
+}
